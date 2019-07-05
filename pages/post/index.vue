5c80141b2452c7e129adfc684b07b91f48b26788
panjianjun <812332110@qq.com>
2019-07-05 11:00:00 +0800

    添加组件

--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
-        攻略首页
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-submenu index="2">
+                <template slot="title">我的工作台</template>
+
+                <el-submenu index="2-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="2-4-1">选项1</el-menu-item>
+                    <el-menu-item index="2-4-2">选项2</el-menu-item>
+                    <el-menu-item index="2-4-3">选项3</el-menu-item>
+                </el-submenu>
+            </el-submenu>
+        </el-menu>
+
     </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
 }
 </script>
 
